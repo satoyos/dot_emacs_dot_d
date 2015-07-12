499b132494868362aedb38c63f4537cb5fa44f92
Yoshifumi Sato <sato_yos@nifty.com>
2015-07-12 12:17:41 +0900

    Introducing anything

--- init.el
+++ init.el
@@ -629,6 +629,15 @@
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ anything.el
+;;; 総合インタフェース
+;;; http://d.hatena.ne.jp/rubikitch/20100718/anything
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+(require 'anything-startup nil t)
+(global-set-key (kbd "M-a")     'anything-for-files)
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; 「Emacsから逃げ出してSublimeText->Atomの後、
 ;;;   Emacsに再入門した際の設定とか記録」より
 ;;; http://blog.bokuweb.me/entry/emcas-nyumon
