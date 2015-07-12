46d8bf32eee0ca1175d61b6ea6261e23df922f00
Yoshifumi sato <sato_yos@nifty.com>
2015-07-12 14:15:04 +0900

    Add motion-mode

--- init.el
+++ init.el
@@ -638,6 +638,21 @@
 (global-set-key (kbd "M-a")     'anything-for-files)
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ motion-mode.el
+;;; RubyMotion開発支援
+;;; https://github.com/ainame/motion-mode
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+(require 'motion-mode)
+;; following add-hook is very important.
+(add-hook 'ruby-mode-hook 'motion-recognize-project)
+(add-to-list 'ac-modes 'motion-mode)
+(add-to-list 'ac-sources 'ac-source-dictionary)
+;; set key-binds as you like
+(define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
+(define-key motion-mode-map (kbd "C-c C-d") 'motion-dash-at-point)
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; 「Emacsから逃げ出してSublimeText->Atomの後、
 ;;;   Emacsに再入門した際の設定とか記録」より
 ;;; http://blog.bokuweb.me/entry/emcas-nyumon
