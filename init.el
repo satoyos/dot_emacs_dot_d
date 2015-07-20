b1a6cd4e7e85af116680d5fde89aeed168d6e619
Yoshifumi Sato <sato_yos@nifty.com>
2015-07-20 19:45:33 +0900

    Add ruby-electric

--- init.el
+++ init.el
@@ -754,6 +754,22 @@
 (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;; Packages for ruby
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+(setq ruby-electric-mode-map
+      (let ((map (make-sparse-keymap)))
+        (define-key map " " 'ruby-electric-space/return)
+        (define-key map [remap delete-backward-char] 'ruby-electric-delete-backward-char)
+        (define-key map [remap newline] 'ruby-electric-space/return)
+        (define-key map [remap newline-and-indent] 'ruby-electric-space/return)
+        map))
+
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
+
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;; @ OS dependency                                                 ;;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
