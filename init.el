6b49d55c0e2fe43f5eff1afdca4910225ce157c1
Yoshifumi Sato <sato_yos@nifty.com>
2015-07-23 22:38:09 +0900

    handle URL in text-mode as a hyperlink

--- init.el
+++ init.el
@@ -770,6 +770,19 @@
 
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;; URLの取り扱い                                                   ;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+;; URLをハイライトで強調表示
+(require 'thingatpt)
+(global-hi-lock-mode 1)
+(add-hook 'text-mode-hook
+		  '(lambda()(highlight-regexp thing-at-point-url-regexp "hi-blue")))
+;; URLをブラウザで開くキーバインド
+(global-set-key "\C-c\C-j" 'browse-url-at-point)
+(global-set-key [double-mouse-1] 'browse-url-at-mouse)
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;; @ OS dependency                                                 ;;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
