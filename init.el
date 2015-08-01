5aac72a1a3d8a47307ffa9c0be6ef7b41e610317
Yoshifumi sato <sato_yos@nifty.com>
2015-08-01 12:40:39 +0900

    Fix error when loading txt-file

--- init.el
+++ init.el
@@ -776,7 +776,7 @@
 ;; URLをハイライトで強調表示
 (require 'thingatpt)
 (global-hi-lock-mode 1)
-(add-hook 'text-mode-hook
+(add-hook 'find-mode-hook
 		  '(lambda()(highlight-regexp thing-at-point-url-regexp "hi-blue")))
 ;; URLをブラウザで開くキーバインド
 (global-set-key "\C-c\C-j" 'browse-url-at-point)
