c379209a48ea1eab35a2f0c3cf45b6d9268e37c2
Yoshifumi sato <sato_yos@nifty.com>
2015-09-27 16:57:31 +0900

    Delete unused code (already comment out)

--- init.el
+++ init.el
@@ -258,40 +258,6 @@
 (set-face-attribute 'linum nil :height 0.75)
 
 
-;; ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
-;; ;;; @ screen - tabbar                                               ;;;
-;; ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
-
-;; (require 'tabbar)
-
-;; ;; tabbar有効化
-;; (call-interactively 'tabbar-mode t)
-
-;; ;; ボタン非表示
-;; (dolist (btn '(tabbar-buffer-home-button
-;;                tabbar-scroll-left-button
-;;                tabbar-scroll-right-button))
-;;   (set btn (cons (cons "" nil) (cons "" nil)))
-;;   )
-
-;; ;; タブ切替にマウスホイールを使用（0：有効，-1：無効）
-;; (call-interactively 'tabbar-mwheel-mode -1)
-;; (remove-hook 'tabbar-mode-hook      'tabbar-mwheel-follow)
-;; (remove-hook 'mouse-wheel-mode-hook 'tabbar-mwheel-follow)
-
-;; ;; タブグループを使用（t：有効，nil：無効）
-;; (defvar tabbar-buffer-groups-function nil)
-;; (setq tabbar-buffer-groups-function nil)
-
-;; ;; タブの表示間隔
-;; (defvar tabbar-separator nil)
-;; (setq tabbar-separator '(1.0))
-
-;; ;; タブ切り替え
-;; (global-set-key (kbd "<C-tab>") 'tabbar-forward-tab)
-;; (global-set-key (kbd "C-q")     'tabbar-backward-tab)
-
-
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; @ search - isearch                                              ;;;
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
