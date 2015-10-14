858bebbd4a92909520661af80172c74942919926
Yoshifumi sato <sato_yos@nifty.com>
2015-10-14 20:33:53 +0900

    Cut out settings about Key-bindings

---
+++ inits/95-key_bindings.el
@@ -0,0 +1,16 @@
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;; @ key binding - keyboard                                        ;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+;; C-hをバックスペースにする
+;; 入力されるキーシーケンスを置き換える
+(keyboard-translate ?\C-h ?\C-?)
+
+;; 段落整形のキーを変更する
+(global-set-key (kbd "C-\\") 'fill-paragraph)
+
+;; 文字列置換
+(global-set-key (kbd "M-r")  'replace-string)
+
+;; コメントの付与と削除
+(global-set-key (kbd "C-/")  'comment-dwim)
