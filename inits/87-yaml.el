8553992f3f72e4fa66114909025eaff68aa3cb2b
Yosifumi Sato <sato_yos@nifty.com>
2017-06-22 18:52:10 +0900

    Add ansible-minor mode to yams-mode

--- inits/87-yaml.el
+++ inits/87-yaml.el
@@ -1,3 +1,5 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 (define-key yaml-mode-map "\C-m" 'newline-and-indent)
+
+(add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
