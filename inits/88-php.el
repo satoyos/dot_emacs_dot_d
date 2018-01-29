cebc6b130096413960394139c705d3f2a87d1c51
Yosifumi Sato <sato_yos@nifty.com>
2018-01-29 20:49:20 +0900

    Add php.el and others

---
+++ inits/88-php.el
@@ -0,0 +1,15 @@
+;; php-mode
+(require 'php-mode)
+
+;; php-mode-hook
+(add-hook 'php-mode-hook
+          (lambda ()
+            (require 'php-completion)
+            (php-completion-mode t)
+            (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
+            (make-local-variable 'ac-sources)
+            (setq ac-sources '(
+                               ac-source-words-in-same-mode-buffers
+                               ac-source-php-completion
+                               ac-source-filename
+                               ))))
